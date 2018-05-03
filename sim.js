70aa30e929f6f1b1045767f7cc5134b9e896a03f
F4Tornado <h.rovnyak@gmail.com>
2018-05-03 10:32:26 -0400

    Walls(very much NOT ish)

--- sim.js
+++ sim.js
@@ -1,6 +1,6 @@
 const players = [];
-const playersStorage = [];
-const playerAmt = 100;
+const playerStorage = [];
+const playerAmt = 50;
 const walls = [];
 let sim;
 let graphW;
@@ -8,6 +8,9 @@ let graphH;
 let graphpos;
 
 function setup() {
+  if (playerAmt % 2) {
+    throw new Error("playerAmt must be an even number.");
+  }
   createCanvas(windowWidth, windowHeight).position(0, 0);
   for (let i = 0; i < playerAmt; i++) {
     console.log("new player");
@@ -31,11 +34,13 @@ function draw() {
   for (let i = walls.length - 1; i >= 0; i--) {
     if (walls[i].show() == "spliceMe") {
       walls.splice(i, 1);
+      walls.push(new wall());
     }
   }
   for (let i = players.length - 1; i >= 0; i--) {
+    players[i].up();
     if (players[i].show() == "spliceMe") {
-      players.splice(i, 1);
+      playerStorage.push(players.splice(i, 1));
     }
   }
   image(sim, graphPos.x, graphPos.y);
@@ -44,12 +49,14 @@ function draw() {
 }
 
 function newGen() {
-  console.log(players);
-  if (players.length <=0) {
+  if (players.length <= 0) {
+    walls.splice(0, walls.length);
+    walls.push(new wall());
     for (let i = 0; i < playerAmt; i++) {
       console.log("new player");
       players.push(new player(new NeuralNetwork(6, 16, 2)));
     }
+    playerStorage.splice(0, playerStorage.length);
   }
 }
 
@@ -65,15 +72,18 @@ const player = function (nn) {
     this.vel-=0.0002;
     this.y-= this.vel;
     console.log(sim.height, this.y-1/sim.height);
-    if (this.y+this.r/2 > 1 || this.y+this.r < 0) {
+    if (this.y+this.r/2 > 1 || this.y+this.r < 0 || collideRectCircle(walls[0].x, walls[0].topY, walls[0].width, walls[0].height, 1/3, this.y, this.r)) {
       return "spliceMe";
     }
   }
   this.run = (y, vel, tyu, w, tyb, dist) => {
-    return this.nn.predict([y, vel, tyu, w, tyb, dist, ryb, ryt]);
+    return this.nn.predict([y, vel, tyu, w, tyb, dist]);
   }
   this.up = () => {
-    this.run(this.y, this.vel, /*top of wall*/ 1, /*height of wall*/ 1, /*bottom of wall*/ 1, /*distance to wall*/ 1);
+    let out = this.run(this.y, this.vel, walls[0].topY, walls[0].height, walls[0].bottomY, walls[0].x-this.x)
+    if (out[0] > out[1]) {
+      vel+=0.001;
+    }
   }
 }
 
@@ -82,11 +92,15 @@ const wall = function() {
   this.bottomY = random(this.topY, 14/16);
   this.height = this.bottomY-this.topY;
   this.width = 1/16;
-  this.x = sim.width;
+  this.x = 1;
   this.show = () => {
-    this.x=-0.1;
-    fill(235, 255);
-    sim.rect(this.x, this.topY*sim.height, this.height*sim.height, this.width*sim.width);
+    this.x-=0.01;
+    sim.noStroke();
+    sim.fill(235, 255);
+    sim.rect(this.x*sim.width, this.topY*sim.height, this.width*sim.width, this.height*sim.height);
+    if (this.x <= 0) {
+      return "spliceMe";
+    }
   }
 }
 
