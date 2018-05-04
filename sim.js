4f2c956b965a6cc13c29e61351b8fdd33a8ae89a
F4Tornado <h.rovnyak@gmail.com>
2018-05-03 20:51:06 -0400

    this error

--- sim.js
+++ sim.js
@@ -31,8 +31,8 @@ function draw() {
   sim = createGraphics(graphW, graphH);
   sim.background(51);
   sim.fill(235);
-  sim.text("FPS: "+round(frameRate()), 10, 15);
-  sim.text("Generations: "+generations, 20+textWidth("FPS: "+round(frameRate())), 15);
+  sim.text("FPS: " + round(frameRate()), 10, 15);
+  sim.text("Generations: " + generations, 20 + textWidth("FPS: " + round(frameRate())), 15);
   for (let i = walls.length - 1; i >= 0; i--) {
     if (walls[i].show() == "spliceMe") {
       walls.splice(i, 1);
@@ -42,7 +42,7 @@ function draw() {
   for (let i = players.length - 1; i >= 0; i--) {
     players[i].up();
     if (players[i].show() == "spliceMe") {
-      playerStorage.push(players.splice(i, 1));
+      playerStorage.push(players.splice(i, 1)[0]);
     }
   }
   image(sim, graphPos.x, graphPos.y);
@@ -56,9 +56,10 @@ function newGen() {
     walls.splice(0, walls.length);
     walls.push(new wall());
     players.sort((a, b) => {
-      return b.score-a.score;
+      return b.score - a.score;
     })
-    for (let i = 0; i < playerAmt/2; i++) {
+    for (let i = 0; i < playerAmt / 2; i++) {
+      console.log(playerStorage[i].nn);
       players.push(playerStorage[i].nn.mutate(mutate));
       players.push(playerStorage[i].nn.mutate(mutate));
     }
@@ -87,10 +88,10 @@ const player = function (nn) {
     sim.noStroke();
     sim.fill(235, 255 / 5);
     sim.ellipse(sim.width / 3, sim.height * this.y, sim.width * this.r, sim.height * this.r);
-    this.vel-=0.0002;
-    this.y-= this.vel;
-    console.log(sim.height, this.y-1/sim.height);
-    if (this.y+this.r/2 > 1 || this.y+this.r < 0 || collideRectCircle(walls[0].x, walls[0].topY, walls[0].width, walls[0].height, 1/3, this.y, this.r)) {
+    this.vel -= 0.0002;
+    this.y -= this.vel;
+    console.log(sim.height, this.y - 1 / sim.height);
+    if (this.y + this.r / 2 > 1 || this.y + this.r < 0 || collideRectCircle(walls[0].x, walls[0].topY, walls[0].width, walls[0].height, 1 / 3, this.y, this.r)) {
       return "spliceMe";
     }
   }
@@ -98,25 +99,25 @@ const player = function (nn) {
     return this.nn.predict([y, vel, tyu, w, tyb, dist]);
   }
   this.up = () => {
-    let out = this.run(this.y, this.vel*100, walls[0].topY, walls[0].height, walls[0].bottomY, walls[0].x-1/3);
+    let out = this.run(this.y, this.vel * 100, walls[0].topY, walls[0].height, walls[0].bottomY, walls[0].x - 1 / 3);
     // console.log(walls[0].x-1/3);
     if (out[0] > out[1]) {
-      this.vel+=0.001;
+      this.vel += 0.001;
     }
   }
 }
 
-const wall = function() {
-  this.topY = random(2/16, 8/16);
-  this.bottomY = random(this.topY, 14/16);
-  this.height = this.bottomY-this.topY;
-  this.width = 1/16;
+const wall = function () {
+  this.topY = random(2 / 16, 8 / 16);
+  this.bottomY = random(this.topY, 14 / 16);
+  this.height = this.bottomY - this.topY;
+  this.width = 1 / 16;
   this.x = 1;
   this.show = () => {
-    this.x-=0.01;
+    this.x -= 0.01;
     sim.noStroke();
     sim.fill(235, 255);
-    sim.rect(this.x*sim.width, this.topY*sim.height, this.width*sim.width, this.height*sim.height);
+    sim.rect(this.x * sim.width, this.topY * sim.height, this.width * sim.width, this.height * sim.height);
     if (this.x <= 0) {
       return "spliceMe";
     }
